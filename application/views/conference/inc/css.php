a8bab3667d63791baff0e353a3b59576d49d4892
Sittipon <pa.sittipon_st@tni.ac.th>
2021-02-10 11:05:32 +0700

    first commit

---
+++ application/views/conference/inc/css.php
@@ -0,0 +1,9 @@
+<style type="text/css">
+    
+    @media only screen and (max-width: 1600px) and (min-width: 1200px) {
+        .section-padding30 {
+            padding-top: 30px;
+            padding-bottom: 30px;
+        }
+    }
+</style>
