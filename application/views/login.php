521706f4bdadfe1a3e295b07114d445ecaaa1699
phakin.n <na.phakin_st@tni.ac.th>
2021-02-28 16:27:05 +0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -8,57 +8,60 @@
 <?php $this->load->view('rooming/inc/css') ?>
 
 <body>
-    <?php   $attr = array('class'=>'user','id'=>'frmlogin','name'=>'frmlogin');
-            echo (isset($_GET['date']) ? 
-            form_open("User/login?date=".$_GET['date'],$attr) : 
-            form_open("User/login",$attr));
+    <?php $attr = array('class' => 'user', 'id' => 'frmlogin', 'name' => 'frmlogin');
+    echo (isset($_GET['date']) ?
+        form_open("User/login?date=" . $_GET['date'], $attr) :
+        form_open("User/login", $attr));
     ?>
+    <div class="form-all">
+        <div class="form-group" style="text-align: center;">
+            <span class="text-header">Sign in</span>
+        </div>
+        <div class="form-group">
+            <?php
+            $attr = array(
+                'class' => 'form-control form-control-user',
+                'autocomplete' => 'off',
+                'placeholder' => 'Enter username',
+                'name' => 'username',
+                'id' => 'username',
+                'value' => set_value("username") ? set_value("username") : ''
+            );
+            echo form_input($attr);
+            ?>
+        </div>
+        <div class="form-group">
+            <?php
+            $attr = array(
+                'class' => 'form-control form-control-user',
+                'placeholder' => 'Enter password',
+                'name' => 'pwd',
+                'id' => 'pwd'
+            );
+            echo form_password($attr);
+            ?>
+        </div>
 
-    <div class="form-group">
         <?php
         $attr = array(
-            'class' => 'form-control form-control-user',
-            'autocomplete' => 'off',
-            'placeholder' => 'username',
-            'name' => 'username',
-            'id' => 'username',
-            'value' => set_value("username") ? set_value("username") : ''
+            'class' => "btn btn-primary btn-user btn-block",
+            'name' => "btn-login",
+            'id' => "btn-login",
+            'value' => "Login"
         );
-        echo form_input($attr);
+        echo form_submit($attr);
         ?>
-    </div>
-    <div class="form-group">
-        <?php
-        $attr = array(
-            'class' => 'form-control form-control-user',
-            'placeholder' => 'password',
-            'name' => 'pwd',
-            'id' => 'pwd'
-        );
-        echo form_password($attr);
+
+        <?php echo form_close() ?>
+        <?php if ($this->session->flashdata('flash_error')) {
         ?>
-    </div>
-
-    <?php
-    $attr = array(
-        'class' => "btn btn-primary btn-user btn-block",
-        'name' => "btn-login",
-        'id' => "btn-login",
-        'value' => "Login"
-    );
-    echo form_submit($attr);
-    ?>
-
-    <?php echo form_close() ?>
-    <?php if ($this->session->flashdata('flash_error')) {
-    ?>
-        <div class="col-12 col-sm-6">
+            <!-- <div class="col-12 col-sm-6"> -->
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <?= $this->session->flashdata('flash_error') ?>
             </div>
-        </div>
-    <?php } ?>
-
+            <!-- </div> -->
+        <?php } ?>
+    </div>
 </body>
 
 </html>
--- application/views/rooming/inc/css.php
+++ application/views/rooming/inc/css.php
@@ -1,12 +1,48 @@
 <style type="text/css">
+    #myCalendarWrapper {
+        width: 40em;
+        border: 1px;
+        padding: 1em 3em;
+        position: relative;
+        font-size: 1rem;
+        margin: auto;
+    }
 
-        #myCalendarWrapper {
-            width: 40em;
-            border: 1px;
-            padding: 1em 3em;
-            position: relative;
-            font-size: 1rem;
-            margin: auto;
-        }
+    .text-header {
+        font-size: 24px;
+        font-weight: 400;
+    }
 
+    #username {
+        margin: auto;
+        width: 90%;
+    }
+
+    #pwd {
+        margin: auto;
+        width: 90%;
+    }
+
+    #btn-login {
+        margin: auto;
+        width: 90%;
+    }
+
+    .form-all {
+        /* transform: translateY(100%); */
+        margin: auto;
+        margin-top: 10%;
+        padding: 30px 10px;
+        width: 400px;
+        border: 1px solid #dadce0;
+        border-radius: 5px;
+        overflow: auto;
+    }
+
+    .alert.alert-danger.alert-dismissible.fade.show {
+        width: 90%;
+        margin: auto;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
 </style>
